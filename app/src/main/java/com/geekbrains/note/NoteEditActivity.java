33b4abd151c5501fadb485c1a59e8cdc960d1aea
geekbrains <chester_095@mail.ru>
2021-09-29 15:30:02 +0300

    Made 2 layout

--- app/src/main/java/com/geekbrains/note/NoteEditActivity.java
+++ app/src/main/java/com/geekbrains/note/NoteEditActivity.java
@@ -1,14 +1,14 @@
 package com.geekbrains.note;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NoteEditActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_note_edit);
     }
 }
