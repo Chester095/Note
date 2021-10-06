1efcc3c551380e98c9e4266ca18bd75362320706
geekbrains <chester_095@mail.ru>
2021-10-06 12:26:05 +0300

    HW 7 start making from activity_note_edit.xml fragment_note_edit.xml

--- app/src/main/java/com/geekbrains/note/ui/NoteEditActivity.java
+++ app/src/main/java/com/geekbrains/note/ui/NoteEditActivity.java
@@ -1,15 +1,22 @@
 package com.geekbrains.note.ui;
 
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
 import static com.geekbrains.note.ui.NotesListFragment.BACK_NAME_EXTRA_KEY;
 import static com.geekbrains.note.ui.NotesListFragment.NAME_EXTRA_KEY;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.geekbrains.note.R;
 import com.geekbrains.note.domain.NoteEntity;
@@ -17,7 +24,7 @@ import com.geekbrains.note.domain.NoteEntity;
 /***
  *
  */
-public class NoteEditActivity extends AppCompatActivity {
+public class NoteEditActivity extends Fragment {
 
     private EditText titleEditText;
     private EditText descriptionEditText;
@@ -27,14 +34,18 @@ public class NoteEditActivity extends AppCompatActivity {
     private NoteEntity noteEntity1;
 
 
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_note_edit);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_note_edit, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         initNoteEdit();
 
-
-        noteEntity1 = getIntent().getParcelableExtra(NAME_EXTRA_KEY);
+        noteEntity1 = getActivity().getIntent().getParcelableExtra(NAME_EXTRA_KEY);
         if (noteEntity1 != null) {
             id = noteEntity1.getId();
             titleEditText.setText(noteEntity1.getTitle());
@@ -44,13 +55,15 @@ public class NoteEditActivity extends AppCompatActivity {
         // создаём новый Entity и что с ней должны сделать
         saveButton.setOnClickListener(this::onClickSaveButton);
         deleteButton.setOnClickListener(this::onClickDeleteButton);
+        super.onViewCreated(view, savedInstanceState);
     }
 
+
     private void initNoteEdit() {
-        titleEditText = findViewById(R.id.title_edit_text);
-        descriptionEditText = findViewById(R.id.description_edit_text);
-        saveButton = findViewById(R.id.save_button);
-        deleteButton = findViewById(R.id.delete_button);
+        titleEditText = getView().findViewById(R.id.title_edit_text);
+        descriptionEditText = getView().findViewById(R.id.description_edit_text);
+        saveButton = getView().findViewById(R.id.save_button);
+        deleteButton = getView().findViewById(R.id.delete_button);
     }
 
     private void onClickSaveButton(View view) {
@@ -61,8 +74,8 @@ public class NoteEditActivity extends AppCompatActivity {
             );
             Intent data = new Intent();
             data.putExtra(BACK_NAME_EXTRA_KEY, noteEntity);
-            setResult(RESULT_OK, data);
-            finish();
+            getActivity().setResult(RESULT_OK, data);
+            getActivity().finish();
         }
     }
 
@@ -74,8 +87,8 @@ public class NoteEditActivity extends AppCompatActivity {
             );
             Intent data = new Intent();
             data.putExtra(BACK_NAME_EXTRA_KEY, noteEntity);
-            setResult(RESULT_CANCELED, data);
+            getActivity().setResult(RESULT_CANCELED, data);
         }
-        finish();
+        getActivity().finish();
     }
 }
