7c6cb447aaa5a2f3e7c60c4424d92470a51deffb
geekbrains <chester_095@mail.ru>
2021-10-08 16:36:44 +0300

    HW 7 start landscape

--- app/src/main/java/com/geekbrains/note/ui/StartActivity.java
+++ app/src/main/java/com/geekbrains/note/ui/StartActivity.java
@@ -2,6 +2,7 @@ package com.geekbrains.note.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 import com.geekbrains.note.R;
@@ -12,9 +13,27 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        if (checkOrientation()) initLandscapeOrientation();
+        else initPortraitOrientation();
+
+    }
+
+    private void initLandscapeOrientation() {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container_main, new NotesListFragment())
+                .replace(R.id.fragment_container_note_edit, new NoteEditFragment())
+                .commit();
+    }
+
+    private void initPortraitOrientation() {
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.fragment_container_main, new NotesListFragment())
                 .commit();
+    }
+
+    private boolean checkOrientation() {
+        return getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
     }
 }
